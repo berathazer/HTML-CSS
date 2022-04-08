d683a8caca2503cdddfce0ddeba7556c4a52f889
berathazer <93477233+berathazer@users.noreply.github.com>
2022-04-09 02:35:30 +0300

    Add files via upload

---
+++ Hotel Site/scripts/script.js
@@ -0,0 +1,5 @@
+let navbar = document.querySelector(".navbar");
+let buton = document.querySelector("#responseBtn");
+buton.addEventListener('click',function(){
+    navbar.classList.toggle('responsive');
+})
